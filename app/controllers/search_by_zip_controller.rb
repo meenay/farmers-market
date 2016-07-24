21d2277f0cb8ae4f17487b856ecb4b8583dadfa2
meenay <meena0288@gmail.com>
2016-07-24 00:10:37 -0700

    updated controller to read url and lookup table and return json

--- app/controllers/search_by_zip_controller.rb
+++ app/controllers/search_by_zip_controller.rb
@@ -2,8 +2,10 @@ class SearchByZipController < ApplicationController
 	#index method responds to HTTP GET
 	def index
 		zip = params["zip"]
+		markets = Market.where(:zip=>zip)
 		#@firstline = Market.first
 		#render :json => @firstline
-		render :json => zip
+		#render :json => zip
+		render :json => markets
 	end
 end
